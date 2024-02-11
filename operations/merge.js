e13c1c7b48e2ecd0717abf1fcee7d2f0efb9e473
SuLin <108602313+alltobebetter@users.noreply.github.com>
2024-02-11 08:11:39 +0800

    Update merge.js

--- operations/merge.js
+++ operations/merge.js
@@ -1,31 +1,13 @@
-// commit.js
-class Commit {
-  constructor(repository, author, message) {
+// merge.js
+class Merge {
+  constructor(repository) {
     this.repository = repository;
-    this.author = author;
-    this.message = message;
-    this.changes = {};
-    this.timestamp = new Date();
   }
 
-  commit() {
-    const commit = {
-      branch: this.repository.currentBranch,
-      author: this.author,
-      message: this.message,
-      changes: { ...this.changes },
-      timestamp: this.timestamp
-    };
-
-    this.repository.commits.push(commit);
-    this.changes = {};
-    console.log(`Committed changes to ${this.repository.currentBranch}`);
-  }
-
-  stage(changes) {
-    this.changes = { ...this.changes, ...changes };
-    console.log('Staged changes:', changes);
+  merge(sourceBranch, targetBranch) {
+    console.log(`Merging ${sourceBranch} into ${targetBranch}`);
+    // Add logic for merging branches
   }
 }
 
-module.exports = Commit;
+module.exports = Merge;
